0afa8e63e6f1bd318721d171cdd11d1c9b135027
Wesley Carr <surfswell17@gmail.com>
2013-07-28 15:36:27 -0400

    refactor

--- hangman.js
+++ hangman.js
@@ -2,6 +2,9 @@ var guess;
 var SECRET_WORD = "ruby";
 var word = "";
 
+var incorrect = [];
+var correct = [];
+
 function DashifyWord () {
   for (var i = 0; i < SECRET_WORD.length; i++) {
     word = word + "_ ";
@@ -18,16 +21,22 @@ var scoreBoard = {
   guess: document.getElementById('guess'),
   word: document.getElementById('word'),
   incorrect: document.getElementById('incorrect')
-
 };
 
 controls.newGameButton.onclick = function () {
   console.log("Start a New Game");
-
+  DashifyWord();
+  scoreBoard.word.innerText = word;
 };
 
 controls.guessButton.onclick = function () {
   console.log("Made a Guess");
   guess = controls.guessBox.value;
+if (SECRET_WORD.indexOf(guess) >= 0) {
+  console.log("guess is in secret word");
+} else {
+  incorrect.push(guess);
+}
+  scoreBoard.incorrect.innerText = incorrect.join(', ');
   scoreBoard.guess.innerText = guess;
 };
